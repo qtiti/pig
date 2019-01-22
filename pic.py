24c3740c3e914fcc11bc83915dbaf1bf6b0ea092
钱甜甜 <qtt@qtt.local>
2019-01-22 20:51:04 +0800

    test

---
+++ pic.py
@@ -0,0 +1,11 @@
+import turtle
+import turtle as t
+import time
+t.color("red", "yellow")
+t.speed(10)
+t.begin_fill()
+for _ in range(50):
+    t.forward(200)
+    t.left(170)
+end_fill()
+time.sleep(1)
